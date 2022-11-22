b8aa33ad6659eba216d3b3140f792f055eb85744
Marcel Guzik <mguzik555@gmail.com>
2022-11-22 16:08:52 +0100

    Cleaned up Main.cpp and changed path to cl kernel
    Path was changed because when running in CMake, original file in source
    dir was inaccessible. To keep things simple, file was copied, but
    `$CMAKE_CURRENT_BINARY_DIR` doesn't refer to actual executable dir, but
    to a build dir, so the path looks like this:
    
    ```
    - string_search_kernel.cl
    - Debug
    + --- HelloOpenCL.exe
    ```
    
    So path had to be changed to accomodate this.

--- gpu/Main.cpp
+++ gpu/Main.cpp
@@ -12,133 +12,88 @@ using std::endl;
 using std::string;
 
 // Helper function to print vector elements
-void printVector(const std::string arrayName,
-                                 const uint8_t * arrayData,
-                                 const unsigned int length)
-{
-    int numElementsToPrint = (256 < length) ? 256 : length;
-    cout << endl << arrayName << ":" << endl;
-    for(int i = 0; i < numElementsToPrint; ++i)
-        printf_s("%hhu ", arrayData[i]);
+template<typename T>
+void printVector(const std::string& arrayName, const std::vector<T>& arrayData) {
+    cout << arrayName << ":\t" << endl;
+    for(int i = 0; i < arrayData.size(); ++i)
+        cout << arrayData[i] << ' ';
     cout << endl;
 }
 
-// Globals
-int length      = 256;
-cl_float * pX   = NULL;
-cl_float * pY   = NULL;
-cl_float a      = 2.f;
-
-std::vector<cl::Platform> platforms;
-cl::Context context;
-std::vector<cl::Device> devices;
-cl::CommandQueue queue;
-cl::Program program;
-
-cl::Kernel kernel;
-cl::Buffer bufX;
-cl::Buffer bufY;
-
-cl::Program createProgramFromFile(cl::Context context, string fileName)
-{
+cl::Program createProgramFromFile(cl::Context& context, const string& fileName) {
     std::ifstream f(fileName);
     std::stringstream st;
     st << f.rdbuf();
     string kernelStr = st.str();
     cl::Program::Sources sources(1, std::make_pair(kernelStr.c_str(), kernelStr.length()));
-    program = cl::Program(context, sources);
+    cl::Program program(context, sources);
     return program;
 }
 
-// Allocate and initialize memory on the host
-void initHost()
-{
-    size_t sizeInBytes = length * sizeof(cl_float);
-    pX = (cl_float *) malloc(sizeInBytes);
-    if(pX == NULL)
-        throw(string("Error: Failed to allocate input memory on host\n"));
-
-    pY = (cl_float *) malloc(sizeInBytes);
-    if(pY == NULL)
-        throw(string("Error: Failed to allocate input memory on host\n"));
-
-    for(int i = 0; i < length; i++)
-    {
-        pX[i] = cl_float(i);
-        pY[i] = cl_float(length-1-i);
-    }
-
-}
-
-// Release host memory
-void cleanupHost()
-{
-    if(pX)
-    {
-        free(pX);
-        pX = NULL;
-    }
-    if(pY != NULL)
-    {
-        free(pY);
-        pY = NULL;
-    }
-}
-
-int main(int argc, char * argv[])
-{
-    try
-    {
-        // Allocate and initialize memory on the host
-        initHost();
-
+int main(int argc, char * argv[]) {
+    cl::Program program;
+    std::vector<cl::Device> devices;
+    try {
         // Find the platform
+        std::vector<cl::Platform> platforms;
         cl::Platform::get(&platforms);
 
         // Create an OpenCL context
         cl_context_properties cps[3] = { CL_CONTEXT_PLATFORM, (cl_context_properties)(platforms[0])(), 0 };
-        context = cl::Context(CL_DEVICE_TYPE_GPU, cps);
+        cl::Context context(CL_DEVICE_TYPE_GPU, cps);
 
         // Detect OpenCL devices
         devices = context.getInfo<CL_CONTEXT_DEVICES>();
 
         // Create an OpenCL command queue
-        queue = cl::CommandQueue(context, devices[0]);
+        cl::CommandQueue queue = cl::CommandQueue(context, devices[0]);
 
         string text = "radosny rabarbar rabat rower rabarbar";
         string keyword = "rabarbar";
         std::vector<cl_uchar> indices(text.size());
 
+        // Create OpenCL memory buffers
         cl::Buffer bufText(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(cl_char) * (int)text.size(), (char*)text.data());
         cl::Buffer bufKeyword(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(cl_char) * (int)keyword.size(), (char*)keyword.data());
         cl::Buffer bufIndices(context, CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR, sizeof(cl_uchar) * (int)indices.size(), indices.data());
 
-        // Create OpenCL memory buffers
-        bufX = cl::Buffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(cl_float) * length, pX);
-        bufY = cl::Buffer(context, CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR, sizeof(cl_float) * length, pY);
-
         // Load CL file, build CL program object, create CL kernel object
-        program = createProgramFromFile(context, "string_search_kernel.cl");
+        // Modified to use a version copied by Cmake into build directory
+        program = createProgramFromFile(context, "../string_search_kernel.cl");
         program.build(devices);
-        kernel = cl::Kernel(program, "string_search");
+        cl::Kernel kernel = cl::Kernel(program, "string_search");
+
         // Set the arguments that will be used for kernel execution
         kernel.setArg(0, bufText);
         kernel.setArg(1, bufKeyword);
         kernel.setArg(2, (unsigned int)keyword.size());
         kernel.setArg(3, bufIndices);
+        
+        // Print the input text
+        printVector("input", std::vector<char>(text.begin(), text.end()));
 
         // Enqueue the kernel to the queue
         // with appropriate global and local work sizes
         queue.enqueueNDRangeKernel(kernel, cl::NDRange(0), cl::NDRange(text.size()));
 
-        // Enqueue blocking call to read back buffer Y
+        // Enqueue blocking call to read back indices buffer
+        //
+        // The output buffer is the same size as the input buffer and contains `1`s at indices where the input text
+        // matches the keyword. It's specifically not a vector of bools, because C++ tries to be smart and optimize
+        // the storage of the vector, basically making it so every bool takes up a bit instead of a byte, but we
+        // explicitly need each element to take up a byte, so that's why we use `uint8_t` here.
+        //
+        // We could potentially make it so that for every byte in the input there is only 1 bit in the output, saving
+        // some space that way, but we'd have to handle that in the kernel itself. Additionally, there can be no more
+        // matches than `len(input) / len(keyword)` (assuming that our matches are guaranteed not to be aliased)
+        // so we can also definitely improve our upper bound here, but I'd like to have some actual measurements before
+        // engaging in potentially premature optimisation.
         std::vector<uint8_t> results(text.size(), 127);
         queue.enqueueReadBuffer(bufIndices, CL_TRUE, 0, sizeof(uint8_t) * results.size(), results.data());
 
-        printVector("indices", results.data(), (unsigned int)results.size());
-
-        // Release host resources
-        cleanupHost();
+        // Another hack to convert `uint8_t` to `uint16_t` because to C++ `uint8_t` is internally char therefore
+        // character
+        printVector("indices", std::vector<uint16_t>(results.begin(), results.end()));
     }
     catch (cl::Error err)
     {
@@ -149,11 +104,9 @@ int main(int argc, char * argv[])
             string str = program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(devices[0]);
             cout << "Program Info: " << str << endl;
         }
-        cleanupHost();
     }
     catch(string msg)
     {
         cerr << "Exception caught in main(): " << msg << endl;
-        cleanupHost();
     }
 }
